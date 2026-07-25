1a3098da3f7e6ffd831933eaa02058432b36d77a
Tharunkumar S <stharunk412@gmail.com>
2026-07-25 15:20:18 +0530

    Add react-icons dependency

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1600, // Silences the >500kB chunk warning
-  },
 })
